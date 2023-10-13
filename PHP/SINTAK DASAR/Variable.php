a22709d0efc18aca79869f76ca3a241f2dd60d0d
ibnurusdianto <ibnu.rusdianto55@gmail.com>
2023-10-13 09:37:51 +0700

    update tugas akhir mySQL

---
+++ PHP/SINTAK DASAR/Variable.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Variable | Sintak Dasar</title>
+</head>
+<body>
+<?php
+    $nama = "Ibnu Rusdianto";
+    $alamat = "Subang, Pusakajaya, Cigugur Kidul";
+    $universitas = "Universitas Pasundan";
+
+    echo "nama anda : $nama";
+    echo "alamat anda : $alamat";
+    echo "universitas anda : $universitas";
+?>
+</body>
+</html>
