dd4a14054423b0d4145e6add5983bd002d0846ce
ibnurusdianto <ibnu.rusdianto55@gmail.com>
2023-10-24 19:57:39 +0700

    menambahkan sesi 30 dan update getJson

--- PHP/Sesi 28 - Selasa, 24 Okt 2023/Latihan Studi Kasus JSON File/GetJson.php
+++ PHP/Sesi 28 - Selasa, 24 Okt 2023/Latihan Studi Kasus JSON File/GetJson.php
@@ -9,25 +9,28 @@
             padding: 10px;
             margin-bottom: 50px;
         }
+
         table {
             border-collapse: collapse;
             width: 80%;
             margin: auto;
         }
+
         th, td {
             text-align: left;
             padding: 8px;
         }
+
         th {
             background-color: #f2f2f2;
         }
+
         tr:nth-child(even) {
             background-color: #FFFFFF;
-
         }
+
         tr:nth-child(odd) {
             background-color: #CCCCCC;
-
         }
     </style>
 </head>
@@ -43,20 +46,34 @@
         <th>Alamat</th>
         <th>Tanggal Lahir</th>
         <th>Nilai</th>
+        <th>Hasil</th>
     </tr>
     </thead>
     <tbody>
     <?php
     $data = file_get_contents("data.json");
     $nilai = json_decode($data, true);
-
     foreach ($nilai as $n) {
+        if ($n['nilai'] >= 90) {
+            $hasil = 'A';
+        } elseif ($n['nilai'] >= 80) {
+            $hasil = 'B';
+        } elseif ($n['nilai'] >= 70) {
+            $hasil = 'C';
+        } elseif ($n['nilai'] >= 65) {
+            $hasil = 'D';
+        } elseif ($n['nilai'] >= 55) {
+            $hasil = 'D';
+        } else {
+            $hasil = 'E';
+        }
         echo "<tr>";
         echo "<td>" . $n['nama'] . "</td>";
         echo "<td>" . $n['kelas'] . "</td>";
         echo "<td>" . $n['alamat'] . "</td>";
         echo "<td>" . $n['tanggal_lahir'] . "</td>";
         echo "<td>" . $n['nilai'] . "</td>";
+        echo "<td>" . $hasil . "</td>";
         echo "</tr>";
     }
     ?>
---
+++ PHP/Sesi 30 - Jumat, 27 Okt 2023/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sesi 30</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+
+</body>
+<script src="js/bootstrap.bundle.js"></script>
+</html>
